0fb5e4869d6afc85ebe3397e61c383be5530dfee
LookasWasTaken~ <piacen.luca@gmail.com>
2023-03-20 16:17:10 +0100

    think it works

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,22 +17,45 @@ const select = document.getElementById("mode_selected");
 // Genero una variabile per lo score
 let score = 0;
 
+// Seleziono l'h3 dello score
+
+const scoreBoard = document.getElementById("score");
+
 // Seleziono l'h3 del risultato
 
-const scoreBoard = document.getElementById("score")
+const result = document.getElementById("result");
+
+// Genero un array vuoto di bombe dinamico
+
+let bombsArray = [];
+
+// Genero una variabile per controllare se una bomba è stata trovata
+let bombFound = false;
 
 // Seleziono il bottone Play da dove dovrà partire un eventListener che genererà una griglia quadrata
 
 const play = document.querySelector(".play");
 
-// Creo un eventListener sul play quando è selezionata la current option
+// Genero un eventListener sul play quando è selezionata la current option
 
 play.addEventListener("click", function () {
   // Seleziono l'option
   let getValue = select.selectedOptions[0].value;
   console.log("Ho premuto Play in modalità:", getValue);
   displayGrid(getValue);
-  scoreBoard.innerHTML = `your current score is: `
+  // Seleziono la length di tutte le .cell
+  const numberCells = document.querySelectorAll(".cell").length;
+  console.log(numberCells, "celle generate");
+  // Svuoto l'array nel caso stessi ripremendo play
+  bombsArray = [];
+  // Al richiamo della funzione, si sostiuisce il return (cit. Fox90)
+  // Richiamo la funzione di generazione delle bombe per riempire l'array vuoto delle bombe
+  bombsArray = createBombs(numberCells);
+  // il console.log dell'array delle bombe, se fosse visibile, darebbe degli aiuti ai giocatori
+  // ***console.log(bombsArray, "posizione della bomba");
+  // Testo dello scoreboard alla generazione delle celle
+  scoreBoard.innerHTML = `your current score is <span>${score}</span>`;
+  result.innerText = "";
 });
 
 // Seleziono il bottone Reset
@@ -121,13 +144,46 @@ function createCell(cellNumber) {
     let cell = document.createElement("div");
     cell.classList.add("cell", cellClass);
     cell.addEventListener("click", function () {
-      cell.classList.add("bg");
-      console.log("Numero Selezionato", cell.innerText);
-      score++;
-      scoreBoard.innerHTML = `your current score is: ${score}`
-      console.log(score, "Score Attuale");
+      if (cell.className.includes("bg")) {
+        return;
+      } else if (bombsArray.includes(Number(cell.innerText))) {
+        cell.style.backgroundColor = "red";
+        bombFound = true;
+        cell.innerHTML = "";
+        cell.innerHTML = `<i class="fa-solid fa-bomb fa-4x tilt"></i>`
+        score.innerText = "";
+        result.innerText = "YOU LOST!";
+        result.classList.add("lost");
+      } else if (bombFound != true) {
+        cell.classList.add("bg");
+        console.log("Numero Selezionato", cell.innerText);
+        score++;
+        scoreBoard.innerHTML = `your current score is <span>${score}</span>`;
+        console.log(score, "Score Attuale");
+        if (score == cellNumber - 99) {
+          result.innerText = "YOU WIN!";
+          result.classList.add("win");
+        }
+      }
     });
     cell.innerHTML = i.toString();
     gridEl.appendChild(cell);
   }
+}
+
+// Funzione che genera le bombe
+
+function createBombs(totalCells) {
+  // Genero un numero randomico tra 1 e ${numberCells}, 16 volte
+  for (let i = 0; i < 16; i++) {
+    let bomb = Math.floor(Math.random() * totalCells) + 1;
+    // Controllo se il numero generato è già dentro l'array delle bombe
+    while (bombsArray.includes(bomb)) {
+      bomb = Math.floor(Math.random() * totalCells) + 1;
+    }
+    bombsArray.push(bomb);
+  }
+  // Ordino gli elementi all'interno dell'array delle bombe in ordine crescente
+  bombsArray.sort((a, b) => a - b);
+  return bombsArray;
 }
